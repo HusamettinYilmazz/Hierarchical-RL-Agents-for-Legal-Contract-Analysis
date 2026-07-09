0e4e5520f46d04cbfb03277c8aee41894c5c8c0f
Husam <husamahmed616@gmail.com>
2026-07-09 19:22:07 +0300

    add supervised finetuning logic

--- agent_train/sft/train_sft.py
+++ agent_train/sft/train_sft.py
@@ -1,63 +1,77 @@
-from datasets import load_dataset
-from transformers import AutoTokenizer
-from transformers import AutoModelForCausalLM
+import os
+os.environ["WANDB_MODE"] = "disabled"
+import sys
+ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, ROOT)
 
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from peft import LoraConfig
+from trl import SFTTrainer, SFTConfig
 
-from trl import SFTTrainer
-from trl import SFTConfig
+from .dataset import create_dataset
+from utils import load_config, Config
 
-MODEL_NAME = "Qwen/Qwen2.5-3B-Instruct"
 
-dataset = load_dataset(
-    "json",
-    data_files="/home/husammm/Desktop/courses/cs_courses/RL/projects/Hierarchical_RL_Agents_for_Legal_Contract_Analysis/agent_train/assets/CUAD/CUAD_v1.json"
-)["train"]
-
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-
-model = AutoModelForCausalLM.from_pretrained(
-    MODEL_NAME,
-    device_map="auto"
-)
-
-def format_example(example):
-
-    return (
-        example["prompt"]
-        + " "
-        + example["answer"]
+def formatting_func(tokenizer, example):
+    text = tokenizer.apply_chat_template(
+        example["messages"],
+        tokenize=False
     )
 
-lora_config = LoraConfig(
-    r=16,
-    lora_alpha=32,
-    lora_dropout=0.05,
-    target_modules=[
-        "q_proj",
-        "k_proj",
-        "v_proj",
-        "o_proj"
-    ]
-)
+    tokens = tokenizer(
+        text,
+        truncation=True,
+        max_length=1024   # or 2048 if GPU  allows
+    )
 
-training_args = SFTConfig(
-    output_dir="./sft_model",
-    num_train_epochs=1,
-    per_device_train_batch_size=1,
-    gradient_accumulation_steps=8,
-    learning_rate=2e-5,
-    logging_steps=10,
-    save_steps=200,
-)
+    return tokenizer.decode(tokens["input_ids"])
 
-trainer = SFTTrainer(
-    model=model,
-    train_dataset=dataset,
-    args=training_args,
-    peft_config=lora_config,
-    formatting_func=format_example
-)
 
-trainer.train()
-trainer.save_model("./sft_model")
+def train(config: Config, checkpoint: str | None = None):
+
+    dataset = create_dataset(json_path=config.data['dataset_path'])
+
+    tokenizer = AutoTokenizer.from_pretrained(config.model['model_name'])
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+
+    model = AutoModelForCausalLM.from_pretrained(
+        config.model['model_name'],
+        device_map="auto"
+    )
+
+    lora_config = LoraConfig(
+        r= config.training['lora_r'],
+        lora_alpha= config.training['lora_alpha'],
+        lora_dropout= config.training['lora_dropout'],
+        bias= config.training['lora_bias'],
+        task_type= config.training['lora_task_type'],
+        target_modules= config.training['lora_target_modules']
+    )
+
+    training_args = SFTConfig(
+        output_dir= os.path.join(ROOT, config.data['output_path'], "sft"),
+        num_train_epochs= config.training['sft_num_train_epochs'],
+        per_device_train_batch_size= config.training['sft_per_device_train_batch_size'],
+        gradient_accumulation_steps= config.training['sft_gradient_accumulation_steps'],
+        learning_rate= float(config.training['sft_learning_rate']),
+        logging_steps= config.training['sft_logging_steps'],
+        save_steps= config.training['sft_save_steps'],
+    )
+
+    trainer = SFTTrainer(
+        model=model,
+        train_dataset=dataset,
+        peft_config=lora_config,
+        args=training_args,
+        formatting_func=formatting_func
+    )
+
+    trainer.train(
+        resume_from_checkpoint= checkpoint
+    )
+    trainer.save_model(os.path.join(ROOT, config.data['output_path']))
+
+if __name__ == "__main__":
+    config = load_config(os.path.join(ROOT, "configs/config.yml"))
+    train(config)

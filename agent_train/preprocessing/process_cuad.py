ad64a291d0c09eeb7b6e468315409e6d2b8e0e24
Husam <husamahmed616@gmail.com>
2026-07-10 21:24:12 +0300

    process cuad for grpo

--- agent_train/preprocessing/process_cuad.py
+++ agent_train/preprocessing/process_cuad.py
@@ -1,57 +1,59 @@
+import os
+
 import json
-from pathlib import Path
+from utils import Config
 
-INPUT_FILE = "/kaggle/input/datasets/konradb/atticus-open-contract-dataset-aok-beta/CUAD_v1/CUAD_v1.json"
-OUTPUT_DIR = Path("/kaggle/working/Hierarchical-RL-Agents-for-Legal-Contract-Analysis/agent_train/outputs")
+def process_data(config: Config, output_dir):
 
-OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+    output_dir.mkdir(parents=True, exist_ok=True)
 
-with open(INPUT_FILE, "r") as f:
-    data = json.load(f)
+    with open(config.data['dataset_path'], "r") as f:
+        data = json.load(f)
 
-examples = []
+    examples = []
 
-for doc in data["data"]:
-    for paragraph in doc["paragraphs"]:
+    for doc in data["data"]:
+        for paragraph in doc["paragraphs"]:
 
-        contract = paragraph["context"]
+            contract = paragraph["context"]
 
-        for qa in paragraph["qas"]:
+            for qa in paragraph["qas"]:
 
-            question = qa["question"]
+                question = qa["question"]
 
-            if len(qa["answers"]) == 0:
-                continue
+                if len(qa["answers"]) == 0:
+                    continue
 
-            answer = qa["answers"][0]["text"]
+                answer = qa["answers"][0]["text"]
 
-            examples.append(
-                {
-                    "prompt":
-                        f"""You are a legal contract analyst.
+                examples.append(
+                    {
+                        "prompt":
+                            f"""You are a legal contract analyst.
 
-Contract:
-{contract}
+    Contract:
+    {contract}
 
-Question:
-{question}
+    Question:
+    {question}
 
-Answer:""",
-                    "answer": answer
-                }
-            )
+    Answer:""",
+                        "answer": answer
+                    }
+                )
 
-split = int(len(examples) * 0.9)
+    split = int(len(examples) * 0.9)
 
-train = examples[:split]
-val = examples[split:]
+    train = examples[:split]
+    val = examples[split:]
 
-with open(OUTPUT_DIR / "train.jsonl", "w") as f:
-    for item in train:
-        f.write(json.dumps(item) + "\n")
+    with open(os.path.join(output_dir, "train.jsonl"), "w") as f:
+        for item in train:
+            f.write(json.dumps(item) + "\n")
 
-with open(OUTPUT_DIR / "val.jsonl", "w") as f:
-    for item in val:
-        f.write(json.dumps(item) + "\n")
+    with open(os.path.join(output_dir, "train.jsonl"), "w") as f:
+        for item in val:
+            f.write(json.dumps(item) + "\n")
 
-print(len(train), len(val))
+    print(len(train), len(val))
+
